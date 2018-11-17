fa80ebed09d8ad297199e2a635bd93e422a16146
Luopm <luo.pingming@ztesoft.com>
2018-11-18 00:34:03 +0800

    app init

---
+++ src/main/java/com/luopm/alumnicircle/AlumniCircleApplication.java
@@ -0,0 +1,12 @@
+package com.luopm.alumnicircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniCircleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlumniCircleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/luopm/alumnicircle/AlumniCircleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.luopm.alumnicircle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlumniCircleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
